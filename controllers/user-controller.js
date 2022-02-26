5ba555f5ae3868db80c11fa7a7dca96955212874
Elvan H <elvanhd@gmail.com>
2022-02-26 15:35:39 +0700

    using the user model for registering

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,23 +1,35 @@
-const uuid = require('uuid').v4;
-
 const DUMMY_USERS = require('../data/users');
 const AppError = require('../errors/app-error');
 const Place = require('../models/place');
+const User = require('../models/user');
 
-exports.registerUser = (req, res, next) => {
+exports.registerUser = async (req, res, next) => {
   const user = req.body;
-  user.id = uuid();
 
-  if (DUMMY_USERS.find((u) => u.email === user.email)) {
-    return next(new AppError('User already exists', 400));
+  try {
+    const existingUser = await User.findOne({ email: user.email });
+
+    if (existingUser) {
+      return next(new AppError('User already exists', 422));
+    }
+
+    const newUser = {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      imageUrl: user.imageUrl,
+      places: [],
+    };
+
+    const createdUser = await User.create(newUser);
+
+    res.json({
+      message: 'User created successfully',
+      user: createdUser,
+    });
+  } catch (error) {
+    return next(new AppError(error, 500));
   }
-
-  DUMMY_USERS.push(user);
-
-  res.status(201).json({
-    message: 'User created successfully',
-    user: user,
-  });
 };
 
 exports.loginUser = (req, res, next) => {
