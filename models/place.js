95fccc86f9550d94ef0979c5047d2b63f34828cf
Elvan H <elvanhd@gmail.com>
2022-02-26 15:54:17 +0700

    adding the relation between places and users

--- models/place.js
+++ models/place.js
@@ -2,17 +2,26 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const placeSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  address: { type: String, required: true },
-  location: {
-    lat: { type: Number, required: true },
-    lng: { type: Number, required: true },
+const placeSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    address: { type: String, required: true },
+    location: {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true },
+    },
+    creator: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  creator: { type: String, required: true },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Place = mongoose.model('Place', placeSchema);
 
--- models/user.js
+++ models/user.js
@@ -3,13 +3,24 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 6 },
-  imageUrl: { type: String, required: true },
-  places: [{ type: String, required: true }],
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    imageUrl: { type: String, required: true },
+    places: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Place',
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.plugin(uniqueValidator);
 
