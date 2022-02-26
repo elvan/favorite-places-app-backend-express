b7a3a4b4db8b9004289bdeba745e4968948819eb
Elvan H <elvanhd@gmail.com>
2022-02-26 13:34:49 +0700

    creating the place model

---
+++ models/place.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const placeSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  creator: { type: String, required: true },
+});
+
+const Place = mongoose.model('Place', placeSchema);
+
+module.exports = Place;
